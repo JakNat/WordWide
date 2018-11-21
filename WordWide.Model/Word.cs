afb12b9849123745b6bee2eaf820cae07abecc3e
kuba.natonek998 <kuba.natonek998@gmail.com>
2018-11-21 20:01:25 +0100

    Set up solution

---
+++ WordWide.Model/Word.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordWide.Model
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public string EnglishWord { get; set; }
+        public string TranslateWord { get; set; }
+        public double Progress { get; set; }
+    }
+}
